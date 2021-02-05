5a06746f6181bfb68190068b6e289b41a3b32cde
Vinicius Gonçalves Martins <vm3090474@gmail.com>
2021-02-05 11:57:26 -0300

    Estruturas de Repetição (While, doWhile e For)

---
+++ aula013/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+/*
+var c = 1
+while (c < 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+
+
+
---
+++ aula014/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai comecar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
