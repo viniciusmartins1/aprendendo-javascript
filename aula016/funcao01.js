ba49cba486588d372f4617d0312f65f7c8c2dd62
Vinicius Gonçalves Martins <vm3090474@gmail.com>
2021-02-10 11:39:18 -0300

    Commit da aula 16 - funções

---
+++ aula016/funcao01.js
@@ -0,0 +1,9 @@
+function parimpa(n){
+    if (n%2 == 0){
+        return 'Par'
+    } else {
+        return 'Impar'
+    }
+}
+
+console.log(parimpa(90))
---
+++ aula016/funcao03.js
@@ -0,0 +1,5 @@
+let v = function dobrar(x) {
+    return x*2
+}
+
+console.log(v(4))
