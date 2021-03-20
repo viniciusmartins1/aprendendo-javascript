a96be18cbef44699f04640b00a11f3c2af0171b9
Vinicius Gonçalves Martins <vm3090474@gmail.com>
2021-03-19 23:48:38 -0300

    2º parte do exercicio da aula 18

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,5 +1,5 @@
 let num = document.getElementById("txtn")
-let lista =  document.getElementById("res")
+let lista =  document.querySelector("select#res")
 let res = document.getElementById("txtres")
 let valores = []
 
@@ -19,10 +19,51 @@ function isLista(n, l) {
     }
 }
 
+function select(elemento) {
+    txtres.innerHTML += `Valor ${elemento.value} adicionado`
+}
+
 function adicionar() {
     if(isNumero(num.value) && !isLista(num.value, valores)){
-        window.alert("Tudo Ok")
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
     } else {
         window.alert("Valor inválido ou já encontrado na lista")
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    for(var valor = 0; valor <= valores.length; valor ++){
+        res.innerHTML += ''
+        res.innerHTML += `Ao todo, temos ${valores.length} numeros cadastrados`
+
+        var maior = valores[0]
+        var menor = valores[0]
+
+        if(valores[valor] > maior){
+            maior = valores[valor]
+        }
+        
+        if(valores[valor] < menor){
+            menor = valores[valor]
+        }
+
+        res.innerHTML += `o maior valor informado foi ${maior}`
+        res.innerHTML += `O menor valor informado foi ${menor}`
+
+        var soma = 0
+        soma += valores[valor]
+
+        res.innerHTML += `Somando os valores, temos ${soma}`
+
+        var media = soma/(valores.length)
+
+        res.innerHTML += `A média dos valores digitados é ${media}`
+
+
     }
 }
